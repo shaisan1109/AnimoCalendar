58b8dd8b5127fbeaec8a28465dfd0e4b89411074
shaisan1109 <shaisan1109@gmail.com>
2024-03-27 15:56:30 +0800

    Made prefs accessible from navbar

--- app/src/main/java/com/mobdeve/s11/mco11/animocalendar/PreferencesFragment.kt
+++ app/src/main/java/com/mobdeve/s11/mco11/animocalendar/PreferencesFragment.kt
@@ -5,15 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
 
-class PreferencesFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_preferences, container, false)
+class PreferencesFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
     }
 
 }
