8718d5c05b092a1b1cdadaebfb9808a03dd2415f
neveTsyUU <126372146+neveTsyUU@users.noreply.github.com>
2024-04-01 00:06:18 +0800

    Create the Function to Cancel Making Event. Will try to do the Save one

--- app/src/main/java/com/mobdeve/s11/mco11/animocalendar/CreateEventActivity.kt
+++ app/src/main/java/com/mobdeve/s11/mco11/animocalendar/CreateEventActivity.kt
@@ -1,12 +1,17 @@
 package com.mobdeve.s11.mco11.animocalendar
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.mobdeve.s11.mco11.animocalendar.databinding.ActivityCreateEventBinding
 
 class CreateEventActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCreateEventBinding
+    private lateinit var cancelAction: Button
+    private lateinit var doAction: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +24,18 @@ class CreateEventActivity : AppCompatActivity() {
 
         val view = binding.root
         setContentView(binding.root)
+
+
+        cancelAction = findViewById(R.id.createEventCancel)
+        doAction = findViewById(R.id.createEventSave)
+
+        cancelAction.setOnClickListener{
+            finish()
+        }
+
+        doAction.setOnClickListener{
+            //send info to database
+        }
+
     }
 }
